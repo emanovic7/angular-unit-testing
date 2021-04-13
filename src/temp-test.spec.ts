0637b05219a790dbd352a5385a8c9d19dcc12020
emanovic7 <e_owusu@live.com>
2021-04-13 10:12:21 -0400

    temp test file

---
+++ src/temp-test.spec.ts
@@ -0,0 +1,18 @@
+import { stat } from "fs";
+
+describe('testing my test', () => {
+
+  let sut;
+  
+  beforeEach(() => {
+    sut = {};
+  })
+
+  it('tests truthiness', () => {
+    sut.a = false;
+    sut.b = true;
+    sut.a = true;
+
+    expect(sut.a).toBe(true);
+  })
+})
