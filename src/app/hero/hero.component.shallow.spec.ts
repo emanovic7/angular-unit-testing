4182b69739c35bd31fa203c46dca9f7f43cded80
emanovic7 <e_owusu@live.com>
2021-04-14 13:31:45 -0400

    adds shallow integration tests

--- src/app/hero/hero.component.shallow.spec.ts
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -3,7 +3,7 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { By } from "@angular/platform-browser";
 import { HeroComponent } from "./hero.component";
 
-describe('heroComponent', () => {
+describe('heroComponent, (shallowTests)', () => {
 
   let fixture: ComponentFixture<HeroComponent>;
 
